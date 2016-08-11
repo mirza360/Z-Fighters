6e305a4bfd031d8110fce9248858b7e439710e32
Abu Mohammad Shabbir Khan <s.h.a.b.b.i.r@live.com>
2016-08-11 18:15:06 +0600

    Tesing xml commit

--- src/Entity/Player.java
+++ src/Entity/Player.java
@@ -1,4 +1,14 @@
 package Entity;
 
-public class Player {
+import Audio.AudioPlayer;
+import TileMap.*;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.util.ArrayList;
+import java.awt.image.BufferedImage;
+import java.util.HashMap;
+
+public class Player extends MapObject{
+
 }
