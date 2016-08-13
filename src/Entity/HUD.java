18cb2438a9d2d8d37b6634458ed849c4ef58a5b1
Abu Mohammad Shabbir Khan <s.h.a.b.b.i.r@live.com>
2016-08-13 20:12:45 +0600

    Changed HUD
    Made it display with rectangles

--- src/Entity/HUD.java
+++ src/Entity/HUD.java
@@ -8,15 +8,22 @@ import javax.imageio.ImageIO;
 public class HUD {
 
     private Player player;
-    private BufferedImage image;
+    private BufferedImage healthImage;
+    private BufferedImage energyImage;
     private Font font;
 
     public HUD(Player p) {
         player = p;
         try {
-            image = ImageIO.read(
+            healthImage = ImageIO.read(
                     getClass().getResourceAsStream(
-                            "/HUD/hud.gif"
+                            "/Sprites/goku_ss/goku_ss_HUD/0.png"
+                    )
+            );
+
+            energyImage = ImageIO.read(
+                    getClass().getResourceAsStream(
+                            "/Sprites/goku_ss/goku_ss_HUD/1.png"
                     )
             );
 
@@ -28,19 +35,38 @@ public class HUD {
     }
 
     public void draw(Graphics2D g) {
-        g.drawImage(image, 0, 10, null);
+        // Draw the HUD images
+        g.drawImage(healthImage, 7, 10, null);
+        g.drawImage(energyImage, 0, 28, null);
+
+        // Draw the HUD rectangle boundary
+        g.setColor(Color.WHITE);
+        g.drawRect(36, 12, 70, 13);
+        g.drawRect(36, 32, 70, 13);
+
+        // Draw the HUD bars
+        g.setColor(Color.GREEN);
+        for(int i = 0; i < player.getHealth(); i++)
+            g.fillRect(36 + (14 * i), 12, 14 + 1, 13 + 1);
+
+        g.setColor(Color.ORANGE);
+        for(int i = 0; i < player.getEnergy() / 100; i++)
+            g.fillRect(36 + (7 * i), 32, 7 + 1, 13 + 1);
+
+        /* For debugging
         g.setFont(font);
         g.setColor(Color.WHITE);
         g.drawString(
                 player.getHealth() + "/" + player.getMaxHealth(),
-                30,
+                60,
                 25
         );
         g.drawString(
                 player.getEnergy() / 100 + "/" +
                         player.getMaxEnergy() / 100,
-                30,
+                52,
                 45
         );
+        */
     }
 }
